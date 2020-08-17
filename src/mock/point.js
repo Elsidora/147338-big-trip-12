07f57009d4a6b73ba9ac5bfc083212af1191ee18
ella <ellasidorova@yandex.ru>
2020-08-18 00:24:06 +0300

    update const.js && mock/point.js

--- src/mock/point.js
+++ src/mock/point.js
@@ -1,321 +1,73 @@
-import {PRICE, CITIES} from "../const";
+import {PRICE, CITIES, TRANSFER, ACTIVITY, OFFERSAVAILABLE} from "../const";
 import {getRandomInteger, shuffle, getRandomBoolean} from "../util";
-const maxDay = 6;
-const maxHour = 23;
-const maxMinute = 59;
+
 const Count = {
   MIN: 1,
   MAX: 5,
 };
 
+/* Переменные, которые отвечают за генерацию дат*/
+const startDay = new Date(Date.now() + 5 * 24 * 60 * 60 * 1000); // Пусть начало путешествия будет всегда через 5 дней
+const startDayTimestamp = startDay.getTime(); // getTime() возвращает числовое значение указанной даты в виде количества миллисекунд, прошедших с 00:00:00 1 января 1970 года по UTC
+const maxDurationDays = 6; // максимальное количество дней продолжительности путешествия
+const maxDurationMilliseconds = maxDurationDays * 24 * 60 * 60 * 1000;
+const endDayTimestamp = getRandomInteger(startDayTimestamp, startDayTimestamp + maxDurationMilliseconds); // получили метку окончания путешествия
+let tempStartPoint = startDayTimestamp; // к этой переменной будет доступ только у функции генерации dateTo
+/* Конец этих переменных */
+
 const generateCityName = () => {
   return CITIES[getRandomInteger(0, CITIES.length - 1)];
 };
 
-const offers = {
-  Taxi: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  Train: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  Ship: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  Transport: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  Drive: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  Flight: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  [`Check-in`]: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  Sightseeing: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-  Restaurant: [
-    {
-      id: `luggage`,
-      title: `Add luggage`,
-      price: 30,
-    },
-    {
-      id: `comfort`,
-      title: `Switch to comfort class`,
-      price: 100,
-    },
-    {
-      id: `meal`,
-      title: `Add meal`,
-      price: 15,
-    },
-    {
-      id: `seats`,
-      title: `Choose seats`,
-      price: 5,
-    },
-    {
-      id: `train`,
-      title: `Travel by train`,
-      price: 30,
-    },
-  ],
-};
-
 const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 
-const getPictureSrc = () => {
-  return `http://picsum.photos/248/152?r=${Math.random()}`;
-};
+const getPictureSrc = () => `http://picsum.photos/248/152?r=${Math.random()}`;
 
-const getRandomPrice = () => {
-  const randomIndex = getRandomInteger(0, PRICE.length - 1);
-  return PRICE[randomIndex];
+const getRandomPrice = () => PRICE[getRandomInteger(0, PRICE.length - 1)];
+
+/* Отдельная функция, чтобы генерировались офферы */
+const getRandomOffers = (count) => shuffle(OFFERSAVAILABLE.slice()).slice(0, count);
+/* Конец отдельной функции */
+
+/* Сделаем отдельную функцию для генерации массива временных точек, чтобы они красивенько располагались друг за другом */
+const getPointDateTo = () => {
+  const tempEndPoint = getRandomInteger(tempStartPoint, Math.floor(tempStartPoint + (endDayTimestamp - tempStartPoint) / 2)); // вычислили время окончания точки
+
+  tempStartPoint = tempEndPoint; // переназначили время старта для следующей генерации
+
+  return tempEndPoint;
 };
+/* Конец красивенькой функции */
 
 /*
-Метод getDate() возвращает
-день месяца указанной даты по местному времени.
+  Вот здесь одно из самых важных изменений: мы экспортируем не функцию создания конкретной точки, а именно функцию отдающую массив со сгенерированными точками.
+  Сделано это как раз для манипуляций с датами:) Так как это данные моковые, никуда наружу этот метод в любом случае смотреть не будет, а main.js мы просто получаем готовый массивчик,
+  как, собственно и будет далее:)
+ */
 
-Метод Date.parse() разбирает строковое представление
-даты и возвращает количество миллисекунд,
-прошедших с 1 января 1970 года 00:00:00 по UTC.
-
-Метод setDate() устанавливает день месяца указанной
-даты по местному времени.
-
-Метод setHours() устанавливает часы указанной даты
-по местному времени и возвращает количество миллисекунд,
-прошедших с 1 января 1970 00:00:00 по UTC до времени,
-представляемого обновлённым экземпляром Date.
-*/
-
-const generateDate = (minDay, minHour, minMinute) => {
-  const daysGap = getRandomInteger(minDay, maxDay);
-  const hoursGap = getRandomInteger(minHour, maxHour);
-  const minutesGap = getRandomInteger(minMinute, maxMinute);
-  const currentDate = new Date();
-
-  currentDate.setDate(currentDate.getDate() - daysGap);
-  currentDate.setHours(currentDate.getHours() - hoursGap);
-  currentDate.setMinutes(currentDate.getMinutes() - minutesGap);
-
-  return currentDate;
-};
-
-export const generateRoutePoint = () => {
-  const types = Object.keys(offers);
+const generateRoutePoint = () => {
+  const types = TRANSFER.concat(ACTIVITY);
   const type = types[getRandomInteger(0, types.length - 1)];
-  const options = shuffle(offers[type].slice());
-  const additionalOptions = options.slice(0, getRandomInteger(0, options.length));
+  /* Получаем отображаемое название типа точки - просто приводим первую букву к верхнему регистру */
+  const typeTitle = type.charAt(0).toUpperCase() + type.slice(1);
+  /* Чистая фантазия - просто рандомно определяем сколько офферов мы хотим, в зависимости от того, какой тип у точки */
+  const offersCount = (TRANSFER.indexOf(type) > -1) ? getRandomInteger(0, OFFERSAVAILABLE.length - 1) : getRandomInteger(0, Math.floor(OFFERSAVAILABLE.length / 2) - 1);
+  /* Конец фантазии:) */
+
   const sentence = shuffle(text.split(`. `).slice());
   const description = sentence.slice(0, getRandomInteger(1, sentence.length)).join(`. `);
 
   const pictures = new Array(getRandomInteger(Count.MIN, Count.MAX)).fill().map(getPictureSrc);
 
-  const dateFrom = generateDate(0, 0, 0, maxDay, maxHour, maxMinute);
-  const dateTo = generateDate(-maxDay / 2, -maxHour / 2, -maxMinute / 2, maxDay / 2, maxHour / 2, maxMinute / 2);
+  const dateFrom = new Date(tempStartPoint);
+  const dateTo = new Date(getPointDateTo()); // здесь мы получаем дату окончания и одновременно обновляем переменную с датой начала следующего промежутка
+
 
   return {
     type,
+    typeTitle,
     cityName: generateCityName(),
-    additionalOptions,
+    additionalOptions: getRandomOffers(offersCount),
     infoDestination: {
       description,
       pictures,
@@ -325,6 +77,10 @@ export const generateRoutePoint = () => {
     dateTo,
     isFavorite: getRandomBoolean(),
   };
+};
+
+export const generatePointsArray = (count) => {
+  return new Array(count).fill().map(generateRoutePoint);
 };
 
 
